8dfa1da50668840785b5414bb10239e24be6bfba
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-09 14:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let panelContainers = document.querySelectorAll(".panel-container");
+let panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    panels.forEach((el) => {
+      el.classList.remove("active");
+      el.parentElement.classList.remove("active");
+    });
+    
+    panel.classList.add("active");
+    panel.parentElement.classList.add("active");
+  });
+});
